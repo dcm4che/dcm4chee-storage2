c2014dc5f3beb3e6d2a829e525f5421b7fff95f8
skroetsch <stevekroetsch@hotmail.com>
2014-11-27 21:12:42 -0500

    add ArchiverService

---
+++ dcm4chee-archiver-service/src/main/java/org/dcm4chee/storage/archiver/service/ArchiverContext.java
@@ -0,0 +1,97 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2012-2014
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4chee.storage.archiver.service;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.dcm4chee.storage.service.ArchiveEntry;
+
+/**
+ * @author Steve Kroetsch<stevekroetsch@hotmail.com>
+ *
+ */
+public class ArchiverContext extends HashMap<ArchiveEntry, Serializable>
+        implements Iterable<ArchiveEntry>, Serializable {
+
+    private static final long serialVersionUID = 984506197890636234L;
+
+    private String groupID;
+    private String name;
+    private String storageSystemID;
+    private String digestAlgorithm;
+
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStorageSystemID() {
+        return storageSystemID;
+    }
+
+    public void setStorageSystemID(String storageSystemID) {
+        this.storageSystemID = storageSystemID;
+    }
+
+    public String getDigestAlgorithm() {
+        return digestAlgorithm;
+    }
+
+    public void setDigestAlgorithm(String digestAlgorithm) {
+        this.digestAlgorithm = digestAlgorithm;
+    }
+
+    @Override
+    public Iterator<ArchiveEntry> iterator() {
+        return this.keySet().iterator();
+    }
+}
