1bf13726e9fa060d05903100bf3bda3532ca594f
Christopher Redekop <chris.redekop@gmail.com>
2014-12-03 16:16:39 -0500

    [#STGMGMT-2] Implement Continuous Delivery for dcm4chee-storage2; differentiate between building from ossrh and deploying to ossrh.

--- .travis.d/install.sh
+++ .travis.d/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 
-mvn -P ossrh versions:update-properties -DincludeProperties=dcm4che.version
-mvn -P ossrh install -DskipTests=true
+mvn -P ossrh-down versions:update-properties -DincludeProperties=dcm4che.version
+mvn -P ossrh-down install -DskipTests=true
