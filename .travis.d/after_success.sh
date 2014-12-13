63c992f8651dc6712cd6b3867b021e9989c2bf5d
Christopher Redekop <chris.redekop@gmail.com>
2014-12-13 08:18:49 -0500

    [STGMGMT-2] Implement Continuous Delivery for dcm4chee-storage2; check for dependencies on oss.sonatype.org when verifying a non-release-candidate.

--- .travis.d/after_success.sh
+++ .travis.d/after_success.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ev
 
-if [[ ${TRAVIS_BRANCH} -eq "master" && ${TRAVIS_PULL_REQUEST} -eq "false" ]]
+if [[ ${RELEASE_CANDIDATE} == "true" ]]
 then
-  echo "update dependencies"
+  BUILD_ID=$(curl -s -H 'Accept: application/vnd.travis-ci.2+json' https://api.travis-ci.org/repos/dcm4che/dcm4chee-arc-cdi/builds | grep -o '^{"builds":\[{"id":[0-9]*' | grep -o '[0-9]*' | tr -d '\n')
+
+  echo "BUILD_ID = ${BUILD_ID}"
+
+  curl -H "Authorization: token ${TRAVIS_TOKEN}" -H 'Accept: application/vnd.travis-ci.2+json' -X POST \
+    https://api.travis-ci.org/builds/${BUILD_ID}/restart
 fi
